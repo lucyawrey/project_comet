10eb2c0a467db0757983dcf16909c9c8fdaa4347
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-12-04 20:56:25 -0500

    added database migrations for player data db

--- api/src/main.rs
+++ api/src/main.rs
@@ -5,17 +5,18 @@ const DATABASE_URL: &str = "sqlite://game_db.sqlite";
 
 #[tokio::main]
 async fn main() {
-    println!("Starting Project Comet Game Data API Service.");
+    println!("☄️ Starting Project Comet Game Data API Service.");
     if !Sqlite::database_exists(DATABASE_URL).await.unwrap_or(false) {
-        println!("Creating database {}.", DATABASE_URL);
+        println!("  Creating database {}.", DATABASE_URL);
         match Sqlite::create_database(DATABASE_URL).await {
-            Ok(_) => println!("Create database success."),
-            Err(error) => panic!("error: {}", error),
+            Ok(_) => println!("  Create database success."),
+            Err(error) => panic!("  error: {}", error),
         }
     } else {
-        println!("Loading existing database.");
+        println!("  Loading existing database.");
     }
     let db = SqlitePool::connect(DATABASE_URL).await.unwrap();
+    println!("  Running database migrations.");
     sqlx::migrate!()
         .run(&db)
         .await.unwrap();
